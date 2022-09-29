9b7580118c2b5f40766c5a136781a32cfb222340
Olivier Rukundo <85986855+orukundo@users.noreply.github.com>
2022-09-29 09:06:17 +0200

    Add files via upload

---
+++ dataset.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 25 07:30:21 2022
+
+Modifications by orukundo@gmail.com Olivier Rukundo
+"""
+
+import os
+from PIL import Image
+from torch.utils.data import Dataset
+import numpy as np
+
+class KUBIACDataset(Dataset):
+    def __init__(self, image_dir, mask_dir, transform=None):
+        self.image_dir = image_dir
+        self.mask_dir = mask_dir
+        self.transform = transform
+        self.images = os.listdir(image_dir)
+        self.maskss = os.listdir(mask_dir)
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, index):
+        img_path = os.path.join(self.image_dir, self.images[index])
+        mask_path = os.path.join(self.mask_dir, self.maskss[index])
+        image = np.array(Image.open(img_path).convert("RGB"))
+        mask = np.array(Image.open(mask_path), dtype=np.float32)
+        mask[mask == 255.0] = 1.0
+
+        if self.transform is not None:
+            augmentations = self.transform(image=image, mask=mask)
+            image = augmentations["image"]
+            mask = augmentations["mask"]
+
+        return image, mask
